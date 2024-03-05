56ea9cacf8b198e41499a5d161a8c62a6d360d97
Lexi Larkin <lexi@qixils.dev>
2024-03-05 17:12:27 -0500

    fix: slightly reduce feed row size on 2p

--- src/graphics/pages/2p.vue
+++ src/graphics/pages/2p.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Round from '../components/Round.vue';
 import FeedRow from '../components/FeedRow.vue';
+
+const feedScale = 0.9;
 </script>
 
 <template>
 	<div class="body">
 		<Round :scale="1" />
-		<FeedRow class="feed" :scale="1" :first-player="0" :timer="true" />
+		<FeedRow class="feed" :scale="feedScale" :first-player="0" :timer="true" />
 	</div>
 </template>
 
@@ -16,7 +18,7 @@ import FeedRow from '../components/FeedRow.vue';
 	flex-flow: column nowrap;
 	justify-content: flex-start;
 	color: white;
-	font-size: 3.5rem;
+	font-size: calc(3.5rem * v-bind(feedScale));
 }
 .feed {
 	width: 100%;
