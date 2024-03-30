5dd335ae4287be5cdfcab30cade51e3a862d3ef8
Lexi Larkin <lexi@qixils.dev>
2024-03-30 00:54:46 -0400

    feat: tweak 4p_16x9

--- src/graphics/pages/4p_16x9.vue
+++ src/graphics/pages/4p_16x9.vue
@@ -2,21 +2,24 @@
 import Round from '../components/Round.vue';
 import FeedRow from '../components/FeedRow.vue';
 import Timer from '../components/Timer.vue';
+
+const infoScale = 1.0
+const feedScale = 0.75
 </script>
 
 <template>
 	<div class="body">
-		<Round :scale="1.1" class="round" />
-		<FeedRow class="feed" :ar="16/9" :scale="0.75" :first-player="0" :timer="false" />
-		<FeedRow class="feed" :ar="16/9" :scale="0.75" :first-player="2" :timer="false" />
-		<Timer :scale="1.1" :dynamic-width="true" />
+		<Round :scale="infoScale" class="round" />
+		<FeedRow class="feed" :ar="16/9" :scale="feedScale" :first-player="0" :timer="false" />
+		<FeedRow class="feed" :ar="16/9" :scale="feedScale" :first-player="2" :timer="false" />
+		<Timer :scale="infoScale" :dynamic-width="true" />
 	</div>
 </template>
 
 <style scoped>
 .body {
-	width: calc(1920px - 27.5rem);
-	font-size: calc(3.5rem  * 0.75);
+	width: calc(1920px - 26rem);
+	font-size: calc(3.5rem  * v-bind(feedScale));
 	color: white;
 }
 </style>
